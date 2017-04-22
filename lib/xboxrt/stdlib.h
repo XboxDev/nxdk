63e2355b1b8c3efcaef529b8b7be7e2cbdc65a45
Matt Borgerson <mborgerson@gmail.com>
2017-04-22 13:08:15 -0700

    Add basic SDL video support (sw render)

--- lib/xboxrt/stdlib.h
+++ lib/xboxrt/stdlib.h
@@ -11,5 +11,7 @@ int sprintf(char *str, const char *fmt, ...);
 
 void *malloc(size_t size);
 void free(void *ptr);
+void *calloc(size_t count, size_t size);
+void *realloc(void *ptr, size_t size);
 
 #endif
