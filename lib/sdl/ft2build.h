47f28e3818710e5d0a2e2ef5b85eb5a42a21e40a
Matt Borgerson <mborgerson@gmail.com>
2017-04-24 02:00:01 -0700

    Build SDL_ttf/freetype

---
+++ lib/sdl/ft2build.h
@@ -0,0 +1,10 @@
+#ifndef FT2_BUILD_MY_PLATFORM_H_
+#define FT2_BUILD_MY_PLATFORM_H_
+
+#define FT2_BUILD_LIBRARY
+#define FT_CONFIG_OPTIONS_H  "ftoption.h"
+#define FT_CONFIG_MODULES_H  "ftmodule.h"
+
+#include <freetype/config/ftheader.h>
+
+#endif /* FT2_BUILD_MY_PLATFORM_H_ */
