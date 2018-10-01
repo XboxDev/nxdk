0d053791b9e8f4f6ba4d724cfee8cab1af348b4d
Stefan Schmidt <thrimbor.github@gmail.com>
2018-10-01 22:21:34 +0200

    xlibc: Add _unimplemented.h

---
+++ lib/xlibc/include/_unimplemented.h
@@ -0,0 +1,6 @@
+#ifndef _XLIBC__UNIMPLEMENTED_H
+#define _XLIBC__UNIMPLEMENTED_H
+
+#define __unimplemented __attribute__((unavailable))
+
+#endif /* end of include guard: _XLIBC__UNIMPLEMENTED_H */
