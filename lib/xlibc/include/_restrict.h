7fc24cb26561db4aaae9b1809a5417de6ac4c36f
Stefan Schmidt <thrimbor.github@gmail.com>
2018-10-01 22:18:08 +0200

    xlibc: Add _restrict.h

---
+++ lib/xlibc/include/_restrict.h
@@ -0,0 +1,10 @@
+#ifndef _XLIBC__RESTRICT_H
+#define _XLIBC__RESTRICT_H
+
+#ifdef __cplusplus
+    #define XLIBC_RESTRICT __restrict
+#else
+    #define XLIBC_RESTRICT restrict
+#endif
+
+#endif /* end of include guard: _XLIBC__RESTRICT_H */
