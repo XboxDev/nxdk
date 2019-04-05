884188be4dbb546fa0136205257ebb96a196a217
Stefan Schmidt <thrimbor.github@gmail.com>
2019-04-05 14:20:22 +0200

    xboxrt: Fix C++ compatibility

--- lib/xboxrt/stat.h
+++ lib/xboxrt/stat.h
@@ -1,6 +1,10 @@
 #ifndef XBOXRT_STAT
 #define XBOXRT_STAT
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #if 0
 int fstat(int fd, struct stat *st);
 int stat(const char *filename, struct stat *st);
@@ -8,5 +12,9 @@ int stat(const char *filename, struct stat *st);
 
 #include <stdint.h>
 typedef uint32_t mode_t;
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
