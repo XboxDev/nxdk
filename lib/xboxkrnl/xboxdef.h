f27dc152b5f3c07838741d8d541fb81a80cb8676
Stefan Schmidt <thrimbor.github@gmail.com>
2019-05-07 03:22:08 +0200

    xboxkrnl: Split out typedefs

---
+++ lib/xboxkrnl/xboxdef.h
@@ -0,0 +1,29 @@
+#ifndef __XBOXDEF_H__
+#define __XBOXDEF_H__
+
+typedef const void *LPCVOID;
+typedef void VOID, *PVOID, *LPVOID;
+typedef PVOID HANDLE, *PHANDLE;
+
+typedef unsigned char BOOLEAN, *PBOOLEAN;
+
+typedef signed char SCHAR, *PSCHAR;
+
+typedef char CHAR, *PCHAR, CCHAR, *LPCH, *PCH, OCHAR, *POCHAR;
+typedef short SHORT, *PSHORT;
+typedef long LONG, *PLONG;
+typedef long long LONGLONG, *PLONGLONG;
+
+typedef unsigned char UCHAR, *PUCHAR;
+typedef unsigned short USHORT, *PUSHORT, CSHORT;
+typedef unsigned short WORD, WCHAR, *PWSTR;
+typedef unsigned int DWORD, *PDWORD, *LPDWORD;
+typedef unsigned long ULONG, *PULONG;
+typedef unsigned long long ULONGLONG;
+
+typedef unsigned int SIZE_T, *PSIZE_T;
+
+typedef int BOOL, *PBOOL;
+typedef const char *PCSZ, *PCSTR, *LPCSTR;
+
+#endif
