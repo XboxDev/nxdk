41a0cedf522249dd4d24d32e0d150d96f7feebb9
Lucas Eriksson <dracc@dracc.se>
2019-05-13 22:23:39 +0200

    led: Update sample, use new LED API

--- samples/led/main.c
+++ samples/led/main.c
@@ -1,19 +1,12 @@
+#include <hal/led.h>
 #include <hal/xbox.h>
-#include <xboxkrnl/xboxkrnl.h>
-
-#define SMC_REG_LEDMODE             0x07
-#define SMC_REG_LEDSEQ              0x08
 
 void main(void)
 {
-    // Set LED to be user controlled
-    HalWriteSMBusValue(0x20, SMC_REG_LEDMODE, FALSE, 0x01);
+    // Set front LEDs to shine green, red and then turned off in a repeating pattern.
+    XSetCustomLED(XLED_GREEN, XLED_RED, XLED_OFF, XLED_OFF);
 
-    // LED pattern is:
-    //   Red:   0xC = 1100 (ON,  ON,  OFF, OFF;   ON,  ON,  OFF, OFF;   ...)
-    //   Green: 0x0 = 0000 (OFF, OFF, OFF, OFF;   OFF, OFF, OFF, OFF;   ...)
-    // This means the red LED will blink slowly and the green LED is off
-    HalWriteSMBusValue(0x20, SMC_REG_LEDSEQ, FALSE, 0xC0);
+    // XResetLED() can be used to restore system control of the front LEDs
 
     // If we quit, the LED might get reset, so we loop forever
     while(1) {
