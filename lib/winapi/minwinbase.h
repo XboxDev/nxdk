a3e172164b29d1e6f645e25b42835bbf5976f403
Stefan Schmidt <thrimbor.github@gmail.com>
2019-07-12 20:50:48 +0200

    xboxkrnl: Move OVERLAPPED typedef to minwinbase

--- lib/winapi/minwinbase.h
+++ lib/winapi/minwinbase.h
@@ -1,9 +1,20 @@
 #ifndef __MINWINBASE_H__
 #define __MINWINBASE_H__
 
+#include <windef.h>
+
 typedef struct _FILETIME {
     DWORD dwLowDateTime;
     DWORD dwHighDateTime;
 } FILETIME;
+
+typedef struct _OVERLAPPED
+{
+    ULONG_PTR Internal;
+    ULONG_PTR InternalHigh;
+    DWORD Offset;
+    DWORD OffsetHigh;
+    HANDLE hEvent;
+} OVERLAPPED, *LPOVERLAPPED;
 
 #endif
