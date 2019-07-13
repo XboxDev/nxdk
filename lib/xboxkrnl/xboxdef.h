e08b417d39197f102bf6e31339a3449e66605002
Stefan Schmidt <thrimbor.github@gmail.com>
2019-07-13 06:53:49 +0200

    xboxkrnl: Move definition of LARGE_INTEGER and ULARGE_INTEGER

--- lib/xboxkrnl/xboxdef.h
+++ lib/xboxkrnl/xboxdef.h
@@ -37,4 +37,42 @@ typedef struct _STRING {
 
 typedef STRING ANSI_STRING, *PANSI_STRING;
 
+/**
+ * MS's way to represent a 64-bit signed int on platforms that may not support
+ * them directly.
+ */
+typedef union _LARGE_INTEGER
+{
+    struct
+    {
+        ULONG LowPart; /**< The low-order 32 bits. */
+        LONG HighPart; /**< The high-order 32 bits. */
+    };
+    struct
+    {
+        ULONG LowPart; /**< The low-order 32 bits. */
+        LONG HighPart; /**< The high-order 32 bits. */
+    } u;
+    LONGLONG QuadPart; /**< A signed 64-bit integer. */
+} LARGE_INTEGER, *PLARGE_INTEGER;
+
+/**
+ * MS's way to represent a 64-bit unsigned int on platforms that may not support
+ * them directly.
+ */
+typedef union _ULARGE_INTEGER
+{
+    struct
+    {
+        ULONG LowPart; /**< The low-order 32 bits. */
+        ULONG HighPart; /**< The high-order 32 bits. */ /**< The high-order 32 bits. */
+    };
+    struct
+    {
+        ULONG LowPart; /**< The low-order 32 bits. */
+        ULONG HighPart; /**< The high-order 32 bits. */
+    } u;
+    ULONGLONG QuadPart; /**< An unsigned 64-bit integer. */
+} ULARGE_INTEGER, *PULARGE_INTEGER;
+
 #endif
