3e35935520b135d1ddf25d89447b82dd6c3d9ea4
Stefan Schmidt <thrimbor.github@gmail.com>
2019-08-05 03:28:54 +0200

    winapi: Implement GetCurrentThreadId()

--- lib/winapi/thread.c
+++ lib/winapi/thread.c
@@ -1,6 +1,11 @@
 #include <processthreadsapi.h>
 #include <xboxkrnl/xboxkrnl.h>
 
+DWORD GetCurrentThreadId (VOID)
+{
+    return (DWORD)((PETHREAD)KeGetCurrentThread())->UniqueThread;
+}
+
 BOOL SwitchToThread (VOID)
 {
     if (NtYieldExecution() != STATUS_NO_YIELD_PERFORMED) {
