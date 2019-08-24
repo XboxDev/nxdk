d09b4963767b04f76f7efdfd999094ee890f409f
Stefan Schmidt <thrimbor.github@gmail.com>
2019-08-24 05:20:59 +0200

    winapi: Implement GetSystemInfo()

--- lib/winapi/sysinfo.c
+++ lib/winapi/sysinfo.c
@@ -10,3 +10,14 @@ void GetSystemTimePreciseAsFileTime (LPFILETIME lpSystemTimeAsFileTime)
     lpSystemTimeAsFileTime->dwLowDateTime = systemTime.LowPart;
     lpSystemTimeAsFileTime->dwHighDateTime = systemTime.HighPart;
 }
+
+void GetSystemInfo (LPSYSTEM_INFO lpSystemInfo)
+{
+    assert(lpSystemInfo != NULL);
+
+    lpSystemInfo->DUMMYUNIONNAME.DUMMYSTRUCTNAME.wProcessorArchitecture = PROCESSOR_ARCHITECTURE_INTEL;
+    lpSystemInfo->dwPageSize = 4096;
+    lpSystemInfo->dwActiveProcessorMask = 1;
+    lpSystemInfo->dwNumberOfProcessors = 1;
+    lpSystemInfo->dwAllocationGranularity = 4096;
+}
--- lib/winapi/sysinfoapi.h
+++ lib/winapi/sysinfoapi.h
@@ -9,6 +9,38 @@ extern "C" {
 
 void GetSystemTimePreciseAsFileTime (LPFILETIME lpSystemTimeAsFileTime);
 
+// Unprovided fields are intentionally disabled to catch code trying to access them
+typedef struct _SYSTEM_INFO
+{
+    union
+    {
+        //DWORD dwOemId;
+        struct
+        {
+            WORD wProcessorArchitecture;
+            WORD wReserved;
+        } DUMMYSTRUCTNAME;
+    } DUMMYUNIONNAME;
+    DWORD dwPageSize;
+    //LPVOID lpMinimumApplicationAddress;
+    //LPVOID lpMaximumApplicationAddress;
+    DWORD_PTR dwActiveProcessorMask;
+    DWORD dwNumberOfProcessors;
+    //DWORD dwProcessorType;
+    DWORD dwAllocationGranularity;
+    //WORD wProcessorLevel;
+    //WORD wProcessorRevision;
+} SYSTEM_INFO, *LPSYSTEM_INFO;
+
+#define PROCESSOR_ARCHITECTURE_INTEL 0
+#define PROCESSOR_ARCHITECTURE_ARM 5
+#define PROCESSOR_ARCHITECTURE_IA64 6
+#define PROCESSOR_ARCHITECTURE_AMD64 9
+#define PROCESSOR_ARCHITECTURE_ARM64 12
+#define PROCESSOR_ARCHITECTURE_UNKNOWN 0xFFFF
+
+void GetSystemInfo (LPSYSTEM_INFO lpSystemInfo);
+
 #ifdef __cplusplus
 }
 #endif
