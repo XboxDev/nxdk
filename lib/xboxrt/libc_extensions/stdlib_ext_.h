d386663f6262f14de858a67d77c8e06cb4cf0eac
Stefan Schmidt <thrimbor.github@gmail.com>
2019-10-02 05:16:15 +0200

    vcruntime: Add pure virtual function call handling

--- lib/xboxrt/libc_extensions/stdlib_ext_.h
+++ lib/xboxrt/libc_extensions/stdlib_ext_.h
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2019 Stefan Schmidt
+ *
+ * Licensed under the MIT License
+ */
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -5,6 +11,11 @@ extern "C" {
 #ifdef _CRT_RAND_S
 int rand_s (unsigned int *randomValue);
 #endif
+
+typedef void (__cdecl *_purecall_handler)(void);
+
+_purecall_handler __cdecl _get_purecall_handler (void);
+_purecall_handler __cdecl _set_purecall_handler (_purecall_handler function);
 
 #ifdef __cplusplus
 }
