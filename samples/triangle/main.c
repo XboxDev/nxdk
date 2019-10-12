efa3a9d6c92cc22892f1da1fc55ab898ddf7dccb
Lucas Eriksson <dracc@dracc.se>
2019-10-12 23:04:35 +0200

    samples: Use new pb_push* in triangle

--- samples/triangle/main.c
+++ samples/triangle/main.c
@@ -101,7 +101,7 @@ int main(void)
         p = pb_begin();
 
         /* Set shader constants cursor at C0 */
-        pb_push1(p, NV097_SET_TRANSFORM_CONSTANT_LOAD, 96); p+=2;
+        p = pb_push1(p, NV097_SET_TRANSFORM_CONSTANT_LOAD, 96);
 
         /* Send the transformation matrix */
         pb_push(p++, NV097_SET_TRANSFORM_CONSTANT, 16);
@@ -191,24 +191,20 @@ static void init_shader(void)
     p = pb_begin();
 
     /* Set run address of shader */
-    pb_push1(p, NV097_SET_TRANSFORM_PROGRAM_START, 0);
-    p += 2;
+    p = pb_push1(p, NV097_SET_TRANSFORM_PROGRAM_START, 0);
 
     /* Set execution mode */
-    pb_push1(p, NV097_SET_TRANSFORM_EXECUTION_MODE,
-        MASK(NV097_SET_TRANSFORM_EXECUTION_MODE_MODE, NV097_SET_TRANSFORM_EXECUTION_MODE_MODE_PROGRAM)
-        | MASK(NV097_SET_TRANSFORM_EXECUTION_MODE_RANGE_MODE, NV097_SET_TRANSFORM_EXECUTION_MODE_RANGE_MODE_PRIV));
-    p += 2;
+    p = pb_push1(p, NV097_SET_TRANSFORM_EXECUTION_MODE,
+                 MASK(NV097_SET_TRANSFORM_EXECUTION_MODE_MODE, NV097_SET_TRANSFORM_EXECUTION_MODE_MODE_PROGRAM)
+                 | MASK(NV097_SET_TRANSFORM_EXECUTION_MODE_RANGE_MODE, NV097_SET_TRANSFORM_EXECUTION_MODE_RANGE_MODE_PRIV));
 
-    pb_push1(p, NV097_SET_TRANSFORM_PROGRAM_CXT_WRITE_EN, 0);
-    p += 2;
+    p = pb_push1(p, NV097_SET_TRANSFORM_PROGRAM_CXT_WRITE_EN, 0);
 
     pb_end(p);
 
     /* Set cursor for program upload */
     p = pb_begin();
-    pb_push1(p, NV097_SET_TRANSFORM_PROGRAM_LOAD, 0);
-    p += 2;
+    p = pb_push1(p, NV097_SET_TRANSFORM_PROGRAM_LOAD, 0);
     pb_end(p);
 
     /* Copy program instructions (16-bytes each) */
@@ -230,13 +226,11 @@ static void init_shader(void)
 static void set_attrib_pointer(unsigned int index, unsigned int format, unsigned int size, unsigned int stride, const void* data)
 {
     uint32_t *p = pb_begin();
-    pb_push1(p, NV097_SET_VERTEX_DATA_ARRAY_FORMAT + index*4,
-        MASK(NV097_SET_VERTEX_DATA_ARRAY_FORMAT_TYPE, format) | \
-        MASK(NV097_SET_VERTEX_DATA_ARRAY_FORMAT_SIZE, size) | \
-        MASK(NV097_SET_VERTEX_DATA_ARRAY_FORMAT_STRIDE, stride));
-    p += 2;
-    pb_push1(p, NV097_SET_VERTEX_DATA_ARRAY_OFFSET + index*4, (uint32_t)data & 0x03ffffff);
-    p += 2;
+    p = pb_push1(p, NV097_SET_VERTEX_DATA_ARRAY_FORMAT + index*4,
+                 MASK(NV097_SET_VERTEX_DATA_ARRAY_FORMAT_TYPE, format) | \
+                 MASK(NV097_SET_VERTEX_DATA_ARRAY_FORMAT_SIZE, size) |  \
+                 MASK(NV097_SET_VERTEX_DATA_ARRAY_FORMAT_STRIDE, stride));
+    p = pb_push1(p, NV097_SET_VERTEX_DATA_ARRAY_OFFSET + index*4, (uint32_t)data & 0x03ffffff);
     pb_end(p);
 }
 
@@ -244,11 +238,11 @@ static void set_attrib_pointer(unsigned int index, unsigned int format, unsigned
 static void draw_arrays(unsigned int mode, int start, int count)
 {
     uint32_t *p = pb_begin();
-    pb_push1(p, NV097_SET_BEGIN_END, mode); p += 2;
+    p = pb_push1(p, NV097_SET_BEGIN_END, mode);
 
-    pb_push(p++,0x40000000|NV097_DRAW_ARRAYS,1); //bit 30 means all params go to same register 0x1810
-    *(p++) = MASK(NV097_DRAW_ARRAYS_COUNT, (count-1)) | MASK(NV097_DRAW_ARRAYS_START_INDEX, start);
+    p = pb_push1(p, 0x40000000|NV097_DRAW_ARRAYS, //bit 30 means all params go to same register 0x1810
+                 MASK(NV097_DRAW_ARRAYS_COUNT, (count-1)) | MASK(NV097_DRAW_ARRAYS_START_INDEX, start));
 
-    pb_push1(p,NV097_SET_BEGIN_END, NV097_SET_BEGIN_END_OP_END); p += 2;
+    p = pb_push1(p, NV097_SET_BEGIN_END, NV097_SET_BEGIN_END_OP_END);
     pb_end(p);
 }
