a0873cb77f63545aa3be6d28b2cb84aeab4ffe88
Stefan Schmidt <thrimbor.github@gmail.com>
2019-11-03 03:54:23 +0100

    vcruntime: Implement type_info class

---
+++ lib/xboxrt/vcruntime/vcruntime_typeinfo.cpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2019 Stefan Schmidt
+ *
+ * Licensed under the MIT License
+ */
+
+#include <string.h>
+#include <vcruntime_typeinfo.h>
+
+extern "C"
+{
+    int __cdecl __std_type_info_compare (const __std_type_info_data *const lhs, const __std_type_info_data *const rhs)
+    {
+        if (lhs == rhs) {
+            return 0;
+        }
+
+        return strcmp(lhs->_DecoratedName + 1, rhs->_DecoratedName + 1);
+    }
+
+    size_t __cdecl __std_type_info_hash (const __std_type_info_data *const data)
+    {
+        // Computes the FNV-1a hash of the mangled name
+        constexpr size_t fnv_offset_basis = 0x811C9DC5;
+        constexpr size_t fnv_prime = 0x1000193;
+
+        size_t hash = fnv_offset_basis;
+        for (auto *it = reinterpret_cast<const unsigned char *>(data->_DecoratedName + 1); *it != '\0'; ++it) {
+            hash ^= static_cast<size_t>(*it);
+            hash *= fnv_prime;
+        }
+
+        return hash;
+    }
+}
---
+++ lib/xboxrt/vcruntime/vcruntime_typeinfo.h
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2019 Stefan Schmidt
+ *
+ * Licensed under the MIT License
+ */
+
+#ifndef __VCRUNTIME_TYPEINFO_H__
+#define __VCRUNTIME_TYPEINFO_H__
+
+#include <assert.h>
+#include <vcruntime_exception.h>
+
+struct __std_type_info_data
+{
+    char const *_UndecoratedName;
+    char const _DecoratedName[1];
+};
+
+extern "C"
+{
+    int __cdecl __std_type_info_compare (const __std_type_info_data *const lhs, const __std_type_info_data *const rhs);
+    size_t __cdecl __std_type_info_hash (const __std_type_info_data *const data);
+}
+
+class type_info
+{
+public:
+    virtual ~type_info ();
+
+    const char *name () const noexcept
+    {
+        // FIXME: return human-readable name
+        assert(false);
+        return NULL;
+    }
+
+    const char *raw_name () const noexcept
+    {
+        return this->_Data._DecoratedName;
+    }
+
+    bool before (const type_info &rhs) const noexcept
+    {
+        return __std_type_info_compare(&this->_Data, &rhs._Data) < 0;
+    }
+
+    size_t hash_code () const noexcept
+    {
+        return __std_type_info_hash(&this->_Data);
+    }
+
+    bool operator== (const type_info &rhs) const noexcept
+    {
+        return __std_type_info_compare(&this->_Data, &rhs._Data) == 0;
+    }
+
+    bool operator!= (const type_info &rhs) const noexcept
+    {
+        return !operator==(rhs);
+    }
+
+private:
+    type_info (type_info const &) = delete;
+    type_info &operator= (type_info const &) = delete;
+
+    mutable __std_type_info_data _Data;
+};
+
+namespace std
+{
+    using ::type_info;
+
+    class bad_cast : public exception
+    {
+    public:
+        bad_cast () noexcept : exception("bad cast", 0)
+        {
+        }
+    };
+
+    class bad_typeid : public exception
+    {
+    public:
+        bad_typeid () noexcept : exception("bad typeid", 0)
+        {
+        }
+    };
+}
+
+#endif
