c922276d10bd44c3a301e3e0b76fbddf01266017
Stefan Schmidt <thrimbor.github@gmail.com>
2019-11-03 19:03:07 +0100

    libc: Add locale.h extensions required by libc++

---
+++ lib/xboxrt/libc_extensions/locale_ext_.h
@@ -0,0 +1,27 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// We don't really have locale support.
+// These are only Windows-specific stubs and placeholders, providing just the bare minimum for libc++.
+
+typedef void *_locale_t;
+
+static _locale_t _create_locale (int category, const char *locale)
+{
+    return NULL;
+}
+
+static void _free_locale (_locale_t locale) {}
+
+#define _ENABLE_PER_THREAD_LOCALE 1
+#define _DISABLE_PER_THREAD_LOCALE 2
+static int _configthreadlocale (int per_thread_locale_type)
+{
+    // We don't support per-thread locales (yet), so we return with a failure code
+    return -1;
+}
+
+#ifdef __cplusplus
+}
+#endif
