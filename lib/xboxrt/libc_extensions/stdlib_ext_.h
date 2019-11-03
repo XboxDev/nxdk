87dff47636659647e07d503dd1e1eb7318ddb0d2
Stefan Schmidt <thrimbor.github@gmail.com>
2019-11-03 18:24:48 +0100

    libc: Provide _sys_nerr macro via stdlib.h

--- lib/xboxrt/libc_extensions/stdlib_ext_.h
+++ lib/xboxrt/libc_extensions/stdlib_ext_.h
@@ -17,6 +17,10 @@ typedef void (__cdecl *_purecall_handler)(void);
 _purecall_handler __cdecl _get_purecall_handler (void);
 _purecall_handler __cdecl _set_purecall_handler (_purecall_handler function);
 
+// Win32 extension providing the highest valid errno number
+// https://docs.microsoft.com/en-us/cpp/c-runtime-library/errno-doserrno-sys-errlist-and-sys-nerr?view=vs-2019
+#define _sys_nerr _PDCLIB_ERRNO_MAX
+
 #ifdef __cplusplus
 }
 #endif
