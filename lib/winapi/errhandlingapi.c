a56ecb748ce4bf5cc8845d505b42e3a87b898696
Stefan Schmidt <thrimbor.github@gmail.com>
2019-11-13 18:01:03 +0100

    winapi: Implement RaiseException()

---
+++ lib/winapi/errhandlingapi.c
@@ -0,0 +1,26 @@
+#include <windows.h>
+#include <string.h>
+#include <xboxkrnl/xboxkrnl.h>
+
+VOID RaiseException (DWORD dwExceptionCode, DWORD dwExceptionFlags, DWORD nNumberOfArguments, const ULONG_PTR *lpArguments)
+{
+    EXCEPTION_RECORD exception_record;
+
+    exception_record.ExceptionCode = dwExceptionCode;
+    exception_record.ExceptionRecord = NULL;
+    exception_record.ExceptionAddress = (PVOID)RaiseException;
+    exception_record.ExceptionFlags = dwExceptionFlags & EXCEPTION_NONCONTINUABLE;
+
+    if (!lpArguments) {
+        exception_record.NumberParameters = 0;
+    } else {
+        if (nNumberOfArguments > EXCEPTION_MAXIMUM_PARAMETERS) {
+            nNumberOfArguments = EXCEPTION_MAXIMUM_PARAMETERS;
+        }
+
+        exception_record.NumberParameters = nNumberOfArguments;
+        memcpy(exception_record.ExceptionInformation, lpArguments, nNumberOfArguments * sizeof(ULONG));
+    }
+
+    RtlRaiseException(&exception_record);
+}
