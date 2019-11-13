1e610f8f1d3909cdb51c87294d0397c6160782ff
Stefan Schmidt <thrimbor.github@gmail.com>
2019-11-13 18:01:03 +0100

    winapi: Implement RaiseException()

---
+++ lib/winapi/errhandlingapi.h
@@ -0,0 +1,17 @@
+#ifndef __ERRHANDLINGAPI_H__
+#define __ERRHANDLINGAPI_H__
+
+#include <winbase.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+VOID RaiseException (DWORD dwExceptionCode, DWORD dwExceptionFlags, DWORD nNumberOfArguments, const ULONG_PTR *lpArguments);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
--- lib/winapi/windows.h
+++ lib/winapi/windows.h
@@ -1,6 +1,7 @@
 #ifndef __WINDOWS_H__
 #define __WINDOWS_H__
 
+#include <errhandlingapi.h>
 #include <fileapi.h>
 #include <handleapi.h>
 #include <memoryapi.h>
