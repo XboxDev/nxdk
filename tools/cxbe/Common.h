0fb34c69bc8da0403da0eea062ceaa509584a59e
Jannik Vogel <email@jannikvogel.de>
2019-12-04 14:41:54 +0100

    cxbe: Move string lengths into constants

---
+++ tools/cxbe/Common.h
@@ -0,0 +1,11 @@
+// Licensed under GPLv2 or (at your option) any later version.
+// Copyright (C) 2019 Jannik Vogel
+// Copyright (C) 2002-2003 Aaron Robinson <caustik@caustik.com>
+
+#ifndef COMMON_H
+#define COMMON_H
+
+#define OPTION_LEN 266
+#define ERROR_LEN 256
+
+#endif
