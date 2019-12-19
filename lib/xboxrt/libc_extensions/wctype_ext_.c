f0497b035ce40596a14c571ae94f281497d392cf
Stefan Schmidt <thrimbor.github@gmail.com>
2019-12-19 08:52:53 +0100

    libc: Add wctype.h stubs required by libc++

---
+++ lib/xboxrt/libc_extensions/wctype_ext_.c
@@ -0,0 +1,75 @@
+#include <assert.h>
+#include <wctype.h>
+
+_PDCLIB_PUBLIC int iswalpha( wint_t wc )
+{
+    assert(0);
+    return 0;
+}
+
+_PDCLIB_PUBLIC int iswblank( wint_t wc )
+{
+    assert(0);
+    return 0;
+}
+
+_PDCLIB_PUBLIC int iswcntrl( wint_t wc )
+{
+    assert(0);
+    return 0;
+}
+
+_PDCLIB_PUBLIC int iswdigit( wint_t wc )
+{
+    assert(0);
+    return 0;
+}
+
+_PDCLIB_PUBLIC int iswlower( wint_t wc )
+{
+    assert(0);
+    return 0;
+}
+
+_PDCLIB_PUBLIC int iswprint( wint_t wc )
+{
+    assert(0);
+    return 0;
+}
+
+_PDCLIB_PUBLIC int iswpunct( wint_t wc )
+{
+    assert(0);
+    return 0;
+}
+
+_PDCLIB_PUBLIC int iswspace( wint_t wc )
+{
+    assert(0);
+    return 0;
+}
+
+_PDCLIB_PUBLIC int iswupper( wint_t wc )
+{
+    assert(0);
+    return 0;
+}
+
+_PDCLIB_PUBLIC int iswxdigit( wint_t wc )
+{
+    assert(0);
+    return 0;
+}
+
+
+_PDCLIB_PUBLIC wint_t towlower( wint_t wc )
+{
+    assert(0);
+    return 0;
+}
+
+_PDCLIB_PUBLIC wint_t towupper( wint_t wc )
+{
+    assert(0);
+    return 0;
+}
