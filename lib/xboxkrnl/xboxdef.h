37e984fd7962ff0c1c98c009175cd3b1d89e0a86
Jannik Vogel <email@jannikvogel.de>
2020-01-15 04:26:27 +0100

    winapi: Add UINT, PUINT and LPUINT types

--- lib/xboxkrnl/xboxdef.h
+++ lib/xboxkrnl/xboxdef.h
@@ -18,6 +18,7 @@ typedef long long LONGLONG, *PLONGLONG;
 typedef unsigned char UCHAR, *PUCHAR;
 typedef unsigned short USHORT, *PUSHORT, CSHORT;
 typedef unsigned short WORD, WCHAR, *PWSTR;
+typedef unsigned int UINT, *PUINT, *LPUINT;
 typedef unsigned long DWORD, *PDWORD, *LPDWORD;
 typedef unsigned long ULONG, *PULONG;
 typedef unsigned long long ULONGLONG;
