a12c8b858df26fcec81549b18bd833e6d380f333
Samuel <samuel.cuella@gmail.com>
2020-01-23 21:28:51 +0100

    winapi: Implement GetSystemTime()

--- lib/winapi/sysinfo.c
+++ lib/winapi/sysinfo.c
@@ -2,6 +2,25 @@
 #include <assert.h>
 #include <xboxkrnl/xboxkrnl.h>
 
+void GetSystemTime (LPSYSTEMTIME lpSystemTime)
+{
+    assert(lpSystemTime != NULL);
+
+    LARGE_INTEGER kTime;
+    TIME_FIELDS timeFields;
+
+    KeQuerySystemTime(&kTime);
+    RtlTimeToTimeFields(&kTime, &timeFields);
+    lpSystemTime->wYear = timeFields.Year;
+    lpSystemTime->wMonth = timeFields.Month;
+    lpSystemTime->wDay = timeFields.Day;
+    lpSystemTime->wHour = timeFields.Hour;
+    lpSystemTime->wMinute = timeFields.Minute;
+    lpSystemTime->wSecond = timeFields.Second;
+    lpSystemTime->wMilliseconds = timeFields.Millisecond;
+    lpSystemTime->wDayOfWeek = timeFields.Weekday;
+}
+
 void GetSystemTimePreciseAsFileTime (LPFILETIME lpSystemTimeAsFileTime)
 {
     assert(lpSystemTimeAsFileTime != NULL);
