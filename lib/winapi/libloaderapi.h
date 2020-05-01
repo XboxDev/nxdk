f364ab38c44aff211eed1e2212ef2187c1133168
Jannik Vogel <email@jannikvogel.de>
2020-05-01 21:33:20 +0200

    winapi: Stub parts of dynamic-linker API

---
+++ lib/winapi/libloaderapi.h
@@ -0,0 +1,26 @@
+#ifndef __LIBLOADERAPI_H__
+#define __LIBLOADERAPI_H__
+
+#include <windef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+HMODULE LoadLibraryExA (LPCSTR lpLibFileName, HANDLE hFile, DWORD dwFlags);
+HMODULE LoadLibraryA (LPCSTR lpLibFileName);
+BOOL FreeLibrary (HMODULE hLibModule);
+FARPROC GetProcAddress (HMODULE hModule, LPCSTR lpProcName);
+
+#ifndef UNICODE
+#define LoadLibraryEx LoadLibraryExA
+#define LoadLibrary LoadLibraryA
+#else
+#error nxdk does not support the Unicode API
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
