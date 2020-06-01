b52fb29319dc2beccd5e4dc81297c79560429bc7
Jannik Vogel <email@jannikvogel.de>
2020-06-01 13:28:25 +0200

    xboxrt: Improve stack-overflow message

--- lib/xboxrt/c_runtime/check_stack.c
+++ lib/xboxrt/c_runtime/check_stack.c
@@ -13,10 +13,12 @@ void _cdecl _xlibc_check_stack (DWORD requested_size, DWORD stack_ptr)
 
     if (requested_size >= stack_ptr || stack_ptr - requested_size < (DWORD)current_thread->StackLimit)
     {
-        _print("\nStack overflow caught!\n"
+        _print("\n"
+               "Stack overflow caught!\n"
                "stack pointer: 0x%x\n"
                "request size:  0x%x\n"
-               "stack limit:   0x%x)\n\n",
+               "stack limit:   0x%x\n"
+               "\n",
                stack_ptr, requested_size, (DWORD)current_thread->StackLimit);
         // TODO: halt execution
     }
