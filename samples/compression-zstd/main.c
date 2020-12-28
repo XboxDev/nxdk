e086ce706e22b0fa118a4db3b35de129353f4062
Apfel <32610623+Apfel@users.noreply.github.com>
2020-12-28 10:47:05 +0100

    zstd: Improved example code

--- samples/compression-zstd/main.c
+++ samples/compression-zstd/main.c
@@ -1,10 +1,8 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include <hal/debug.h>
 #include <hal/video.h>
-#include <hal/input.h>
 #include <hal/xbox.h>
 
 #include <xboxkrnl/xboxkrnl.h>
@@ -16,28 +14,32 @@ const char* example = "This text has been compressed on the Microsoft Xbox using
 int main(void)
 {
     XVideoSetMode(640, 480, 32, REFRESH_DEFAULT);
+    debugPrint("Compressing now:\n\"%s\"\n", example);
 
     size_t size = strlen(example), compressed_size = ZSTD_compressBound(strlen(example));
+    debugPrint("Original size: %lu.\n", size);
 
     char* compressed_buffer = calloc(compressed_size + 1, sizeof(char));
-    memset(compressed_buffer, 0, (compressed_size + 1) * sizeof(char));
+    size_t end_size = ZSTD_compress(compressed_buffer, compressed_size, example, size, 16);
+    debugPrint("New size: %lu.\n", end_size);
 
-    int code = ZSTD_compress(compressed_buffer, compressed_size, example, size, 16);
-    debugPrint("Compression result code: %d\n", code);
-    debugPrint("Original size: %lu\n", size);
-    debugPrint("Compressed size: %lu\n", compressed_size);
+    char* minified_compressed_buffer = calloc(end_size + 1, sizeof(char));
+    memcpy(minified_compressed_buffer, compressed_buffer, sizeof(char) * end_size);
+    free(compressed_buffer);
 
     debugPrint("Compressed data:\n");
-    debugPrintHex(compressed_buffer, compressed_size);
+    debugPrintHex(minified_compressed_buffer, end_size);
     debugPrint("\n");
 
-    char* decompressed_buffer = calloc(size + 1, sizeof(char));
-    code = ZSTD_decompress(decompressed_buffer, size, compressed_buffer, compressed_size);
-    debugPrint("Decompression result code: %d\n", code);
+    char* decompressed_buffer = calloc(1, sizeof(char));
+    size_t new_size = ZSTD_decompress(decompressed_buffer, size, minified_compressed_buffer, end_size);
+    debugPrint("Decompressed size: %d.\n", new_size);
     debugPrint("Decompressed data:\n\"%s\"\n", decompressed_buffer);
 
+    free(minified_compressed_buffer);
     free(decompressed_buffer);
-    free(compressed_buffer);
+
+    debugPrint("Done.");
 
     while (TRUE) NtYieldExecution();
 
