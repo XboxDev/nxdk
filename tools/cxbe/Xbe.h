fba3e1c8e6c49cfa410958589b4739ce753f986f
Stefan Schmidt <thrimbor.github@gmail.com>
2021-09-13 05:10:53 +0200

    cxbe: Update license statements, fix spelling

--- tools/cxbe/Xbe.h
+++ tools/cxbe/Xbe.h
@@ -21,10 +21,8 @@
 // *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // *  GNU General Public License for more details.
 // *
-// *  You should have recieved a copy of the GNU General Public License
-// *  along with this program; see the file COPYING.
-// *  If not, write to the Free Software Foundation, Inc.,
-// *  59 Temple Place - Suite 330, Bostom, MA 02111-1307, USA.
+// *  You should have received a copy of the GNU General Public License
+// *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // *
 // *  (c) 2002-2003 Aaron Robinson <caustik@caustik.com>
 // *
