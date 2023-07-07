da6a0bd73ef141dca46bd69defba482ebdbc0dd3
Erik Abair <erik.abair@gmail.com>
2023-07-06 21:04:55 -0700

    cxbe: Add clang-format

--- tools/cxbe/Common.h
+++ tools/cxbe/Common.h
@@ -9,17 +9,17 @@
 #define OPTION_LEN 266
 #define ERROR_LEN 256
 
-struct Option {
+struct Option
+{
     char *value;
     const char *key;
     const char *desc;
 };
 
-int ParseOptions(char *argv[], int argc,
-                 const Option *options, char *szErrorMessage);
+int ParseOptions(char *argv[], int argc, const Option *options, char *szErrorMessage);
 void ShowUsage(const char *program, const char *desc, const Option *options);
-int GenerateFilename(char *szNewPath, const char *szNewExtension,
-                     const char *szOldPath, const char *szOldExtension);
+int GenerateFilename(char *szNewPath, const char *szNewExtension, const char *szOldPath,
+                     const char *szOldExtension);
 bool CompareString(const char *szA, const char *szB);
 
 #endif
