19195a00f93d7fe540174f181d8ac0516c27c260
Ryzee119 <wendland@live.com.au>
2024-05-29 18:18:31 +0930

    nvnetdrv: Soft error if framing error only

--- lib/net/nvnetdrv/nvnetdrv.c
+++ lib/net/nvnetdrv/nvnetdrv.c
@@ -201,7 +201,7 @@ static void nvnetdrv_handle_rx_irq (void)
             goto release_packet;
         }
 
-        if (!(flags & NV_RX_ERROR) || (flags & NV_RX_FRAMINGERR)) {
+        if ((flags & NV_RX_ERROR_MASK) == 0) {
             uint16_t packet_length = rx_packet->length;
 
             if (flags & NV_RX_SUBTRACT1) {
