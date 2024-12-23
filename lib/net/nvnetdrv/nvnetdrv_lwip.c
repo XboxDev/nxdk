08ffb7f00d69ce5a4e37221bb06b78c71c32f6de
Ryan Wendland <wendland@live.com.au>
2024-12-24 09:30:59 +1030

    lwip: Move hardware init to after lwip prep

--- lib/net/nvnetdrv/nvnetdrv_lwip.c
+++ lib/net/nvnetdrv/nvnetdrv_lwip.c
@@ -92,16 +92,6 @@ void rx_callback (void *buffer, uint16_t length)
  */
 static err_t low_level_init (struct netif *netif)
 {
-    if (nvnetdrv_init(RX_BUFF_CNT, rx_callback, PBUF_POOL_SIZE) < 0) {
-        return ERR_IF;
-    }
-
-    /* set MAC hardware address length */
-    netif->hwaddr_len = ETHARP_HWADDR_LEN;
-
-    /* set MAC hardware address */
-    memcpy(netif->hwaddr, nvnetdrv_get_ethernet_addr(), 6);
-
     /* maximum transfer unit */
     netif->mtu = 1500;
 
@@ -122,6 +112,16 @@ static err_t low_level_init (struct netif *netif)
     }
 #endif /* LWIP_IPV6 && LWIP_IPV6_MLD */
     g_pnetif = netif;
+
+    if (nvnetdrv_init(RX_BUFF_CNT, rx_callback, PBUF_POOL_SIZE) < 0) {
+        return ERR_IF;
+    }
+
+    /* set MAC hardware address length */
+    netif->hwaddr_len = ETHARP_HWADDR_LEN;
+
+    /* set MAC hardware address */
+    memcpy(netif->hwaddr, nvnetdrv_get_ethernet_addr(), 6);
 
     return ERR_OK;
 }
